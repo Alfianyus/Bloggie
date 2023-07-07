fbd5ee7b6044edc089a547b288b56c9d72d64fae
alfian.yuska <alfian.yuska@phiraka.com>
2023-07-07 10:58:27 +0700

    UpdateBloggieDBContext

---
+++ Bloggie.web/Data/BloggieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Bloggie.web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ Bloggie.web/Models/Domain/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace Bloggie.web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
